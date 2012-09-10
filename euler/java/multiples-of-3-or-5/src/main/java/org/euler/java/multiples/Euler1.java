e4b0b62c42f1c20a19821581121e83dcdbe38ec7
Jonathan Monette <jmonette@homeaway.com>
2012-09-10 16:44:14 -0500

    Javadocing the first project. Minor typo

--- euler/java/multiples-of-3-or-5/src/main/java/org/euler/java/multiples/Euler1.java
+++ euler/java/multiples-of-3-or-5/src/main/java/org/euler/java/multiples/Euler1.java
@@ -1,14 +1,21 @@
 package org.euler.java.multiples;
 
 /**
+ * Solves the first euler problem from projecteuler.net
  * @author jmonette
  */
 public class Euler1 {
     public static void main(String[] args) {
-        System.out.println(sumMultiplesOf3Or9(1000));
+        System.out.println(sumMultiplesOf3Or5(1000));
     }
 
-    public static int sumMultiplesOf3Or9(final int bound) {
+    /**
+     * Add all the natural numbers below one thousand that are multiples of 3 or 5.
+     *
+     * @param bound The bound to follow
+     * @return The sum of the multiples of 3 or 5 under the bound
+     */
+    public static int sumMultiplesOf3Or5(final int bound) {
         int sum = 0;
         for (int i = 1; i < bound; ++i) {
             if (i % 3 == 0 || i % 5 == 0) { sum += i; }
--- euler/java/multiples-of-3-or-5/src/test/java/org/euler/java/multiples/Euler1Test.java
+++ euler/java/multiples-of-3-or-5/src/test/java/org/euler/java/multiples/Euler1Test.java
@@ -6,17 +6,25 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * Unit test class
+ *
  * @author jmonette
  */
 public class Euler1Test {
 
+    /**
+     * Basic test. What is the sum under 10: example given on http://projecteuler.net/problem=1
+     */
     @Test
     public void basicTest() {
-        assertThat(Euler1.sumMultiplesOf3Or9(10), is(23));
+        assertThat(Euler1.sumMultiplesOf3Or5(10), is(23));
     }
 
+    /**
+     * Pass test. Part of problem description
+     */
     @Test
     public void passTest() {
-        assertThat(Euler1.sumMultiplesOf3Or9(1000), is(233168));
+        assertThat(Euler1.sumMultiplesOf3Or5(1000), is(233168));
     }
 }
